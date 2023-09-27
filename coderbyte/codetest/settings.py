370c6eccda511a4d0b5ce100c1d875abbcbb72ed
sachinsinha1304 <sinha6031@gmail.com>
2023-09-28 00:24:49 +0530

    Deploying Changes

--- coderbyte/codetest/settings.py
+++ coderbyte/codetest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-09eqp$gjqk)z)5ndgmd__o9d93o54d39qbk^5pb&3gmxnv1_t$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (BASE_DIR,os.path.join(BASE_DIR,'static'))
-STATIC_ROOT = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
